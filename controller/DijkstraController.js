3a1b53fb14e05f014a2d1bcc56d3570b1065e030
lpiem <Loic-0607@hotmail.fr>
2018-12-13 11:28:38 +0100

    [FEAT] Add dijkstra to kml

--- controller/DijkstraController.js
+++ controller/DijkstraController.js
@@ -1,16 +1,15 @@
-var nodeDijkstra = require("node-dijkstra");
-var sqlite3 = require("sqlite3").verbose();
-var db = new sqlite3.Database("lp_iem_sig.sqlite");
 var kml = require("tokml");
-var Point = require("../model/point.js");
 var Graph = require("node-dijkstra");
-
+var createTableAdj = require("../utils/create-table-adj.js");
+var getAllPoint = require("../utils/getAllPoints.js");
+var write = require("write-file");
 
 var DijkstraController = {
   dijkstra: async (req, res) => {
     let points = await createTableAdj();
     let listAllPoints = await getAllPoint();
-
+    let pointDepart = req.query.first ? `${req.query.first}` : `1`;
+    let pointFin = req.query.end ? `${req.query.end}` : `180`;
     let stringGraph = "{";
 
     let counter2 = 0;
@@ -23,14 +22,13 @@ var DijkstraController = {
           if (val.GEO_POI_ID == point.sommet) {
             sommet1 = val;
           } else if (val.GEO_POI_ID == arc) {
-            sommet2 = val
+            sommet2 = val;
           }
-        })
+        });
 
-        let distance = calculDistance(sommet1, sommet2)
+        let distance = calculDistance(sommet1, sommet2);
         if (distance == 0) {
         } else {
-
           if (counter == point.arc.length - 1) {
             stringGraph += `\"${arc}\":${distance}`;
           } else {
@@ -38,104 +36,86 @@ var DijkstraController = {
           }
         }
         counter++;
-      })
-      if(stringGraph[stringGraph.length-1] == ","){
-        stringGraph = stringGraph.substring(0,stringGraph.length-1)
+      });
+      if (stringGraph[stringGraph.length - 1] == ",") {
+        stringGraph = stringGraph.substring(0, stringGraph.length - 1);
       }
       if (counter2 == points.length - 1) {
         stringGraph += "}";
       } else {
         stringGraph += "},";
       }
-      counter2++
-    })
+      counter2++;
+    });
 
     stringGraph += "}";
 
-    let jsonGraph
+    let jsonGraph;
     try {
       jsonGraph = JSON.parse(stringGraph);
     } catch (e) {
-      throw e
+      throw e;
     }
 
     const route = new Graph(jsonGraph);
 
-    let path = route.path("123", "120");
+    let path = route.path(pointDepart, pointFin);
 
-    res.json(path);
-  }
-};
+    let geojson = {
+      type: "FeatureCollection",
+      name: "Mon parcours",
+      features: [
+        {
+          type: "Feature",
+          properties: {},
+          geometry: {
+            type: "LineString",
+            coordinates: []
+          }
+        }
+      ]
+    };
 
-async function createTableAdj() {
-  points = new Array();
-  await new Promise((resolve, reject) => {
-    db.all("SELECT * FROM GEO_POINT WHERE GEO_POI_ID", function (err, rows) {
+    geojson.name = "Dijkstra";
+
+    path.forEach(res => {
+      let LatLong = new Array();
+      let pointPlace = {
+        type: "Feature",
+        properties: {
+          name: ""
+        },
+        geometry: {
+          type: "Point",
+          coordinates: []
+        }
+      };
+      listAllPoints.forEach(point => {
+        if (point.GEO_POI_ID == res) {
+          LatLong.push(point.GEO_POI_LONGITUDE);
+          LatLong.push(point.GEO_POI_LATITUDE);
+          pointPlace.properties.name = point.GEO_POI_NOM;
+        }
+      });
+      pointPlace.geometry.coordinates.push(LatLong);
+      geojson.features[0].geometry.coordinates.push(LatLong);
+      geojson.features.push(pointPlace);
+    });
+
+    let resultKml = kml(geojson);
+
+    write("output/" + geojson.name + ".kml", resultKml, err => {
       if (err) {
         throw err;
       }
-
-      rows.forEach(row => {
-        point = new Point(row.GEO_POI_ID);
-
-        points.push(point);
-      });
-      resolve();
     });
-  });
 
-  await new Promise((resolve, reject) => {
-    let i = 1;
-    points.forEach(async point => {
-      await new Promise((resolve, reject) => {
-        db.all(
-          `SELECT * FROM GEO_ARC WHERE GEO_ARC_DEB = ${
-          point.sommet
-          } OR GEO_ARC_FIN = ${point.sommet}`,
-          function (err, values) {
-            if (err) {
-              throw err;
-            }
-            values.forEach(value => {
-              if (value.GEO_ARC_DEB == point.sommet) {
-                point.addArc(value.GEO_ARC_FIN);
-              }
-              if (value.GEO_ARC_FIN == point.sommet) {
-                point.addArc(value.GEO_ARC_DEB);
-              }
-            });
-
-            resolve();
-          }
-        );
-      });
-      if (i == points.length) {
-        resolve();
-      }
-      i++;
-    });
-  });
-  return points;
-}
-
-async function getAllPoint() {
-  let points;
-  await new Promise(resolve => {
-    db.all("SELECT * FROM GEO_POINT", function (
-      err,
-      rows
-    ) {
-      points = rows
-      resolve();
-    });
-  });
-  return points;
-}
+    res.type("application/xml");
+    res.send(resultKml);
+  }
+};
 
 function calculDistance(sommet1, sommet2) {
-  //console.log(sommet1);
-  //console.log(sommet2);
-
   const n = 0.7289686274,
     C = 11745793.39,
     e = 0.08248325676,
@@ -144,8 +124,6 @@ function calculDistance(sommet1, sommet2) {
 
   let gamma0 = 3600 * 2 + 60 * 20 + 14.025;
   gamma0 = (gamma0 / (180 * 3600)) * Math.PI;
-
-  // console.log(gamma0);
 
   let lat1 = sommet1.GEO_POI_LATITUDE * 3600;
   let lon1 = sommet1.GEO_POI_LONGITUDE * 3600;
@@ -175,14 +153,8 @@ function calculDistance(sommet1, sommet2) {
   let Lx2 = Xs + R2 * Math.sin(gamma2);
   let Ly2 = Ys - R2 * Math.cos(gamma2);
 
-  //console.log("X1", Lx1);
-  //console.log("Y1", Ly1);
-  //console.log("X2", Lx2);
-  //console.log("Y2", Ly2);
-
   let distance = Math.sqrt(Math.pow(Lx2 - Lx1, 2) + Math.pow(Ly2 - Ly1, 2));
 
-  //console.log(distance)
   return distance;
 }
 
